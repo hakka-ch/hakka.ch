6dd9d5a320efe4bdb2e95d0c57d1aedfcdc92d13
Rokuhashi <inekomugi@gmail.com>
2021-04-14 22:51:10 +0900

    ソート機能の追加

--- assets/js/searchClothet.js
+++ assets/js/searchClothet.js
@@ -3,6 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const itemsEle = document.querySelectorAll('.clothet__item');
   const itemsArr = Array.from(itemsEle);
 
+  const selector = document.querySelector('select.clothet-sort');
+
+  const sortOption = [
+    // [, 'default', 'デフォルト'],
+    [true, 'name', 'なまえ （昇順）'],
+    [false, 'name', 'なまえ （降順）'],
+    [true, 'author', '作者 （昇順）'],
+    [false, 'author', '作者 （降順）'],
+    [false, 'release', 'リリース日時 （新しい）'],
+    [true, 'release', 'リリース日時 （古い）'],
+  ];
+
   function findItems (word) {
     word = word.toLowerCase();
     const found = itemsArr.filter(function (e, i) {
@@ -21,12 +33,19 @@ document.addEventListener('DOMContentLoaded', function() {
     by = by ? by : 'name';
     const sorted = itemsArr.sort(function (a, b) {
       const sort = ascOrder ? 1 : -1;
-      aName = a.querySelector('.item__' + by + ' span');
-      bName = b.querySelector('.item__' + by + ' span');
-      if (aName.innerText > bName.innerText) {
+      const comparison = (i) => {
+        if (by == 'name' || by == 'author') return i.dataset[by]
+        if (by == 'release') {
+          if (i.dataset[by].length < 1) {
+            return new Date("2000-01-01");
+          }
+          return new Date(i.dataset[by]);
+        }
+      };
+      if (comparison(a) > comparison(b)) {
         return sort;
       }
-      if (aName.innerText < bName.innerText) {
+      if (comparison(a) < comparison(b)) {
         return - sort;
       }
     });
@@ -35,6 +54,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     return sorted;
   }
+
+  function initSelector () {
+
+    const fragment = new DocumentFragment();
+    sortOption.forEach(i => {
+      const o = document.createElement('option');
+      const t = document.createTextNode(i[2]);
+      o.setAttribute('value', i[2]);
+      o.appendChild(t);
+
+      fragment.appendChild(o);
+    });
+    selector.appendChild(fragment);
+    sortItems(false, 'release');
+    selector.selectedIndex = 4;
+  }
+  initSelector();
+
+  selector.addEventListener('change', function (e) {
+    const v = e.target.value;
+    const i = sortOption.findIndex(i => i[2] == v);
+    sortItems(sortOption[i][0], sortOption[i][1]);
+  });
 
   /**
    * カテゴリで絞るやつ
